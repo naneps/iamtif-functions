f8abd97af11c7802183ccf7cba122d061c4ad1af
naneps <ekaprasetya2244@gmail.com>
2023-11-19 16:27:22 +0700

    actions setup

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,33 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Function untuk membuat user baru setelah registrasi berhasil
+exports.createUserOnSignUp = functions.auth.user().onCreate((user) => {
+  const {uid, email, displayName} = user;
+
+  // Tambahkan informasi pengguna ke Firestore atau basis data Anda
+  return admin.firestore().collection("users").doc(uid).set({
+    email,
+    displayName,
+    createdAt: admin.firestore.FieldValue.serverTimestamp(),
+  });
+});
+
+// Fungsi utama yang memerlukan otentikasi
+exports.mainFunction = functions.https.onCall(async (data, context) => {
+  // Periksa apakah pengguna telah masuk
+  if (!context.auth) {
+    return {error: "Not authenticated"};
+  }
+
+  try {
+    // Lakukan operasi utama di sini
+    const result = "Main function executed successfully!";
+
+    return {result};
+  } catch (error) {
+    console.error("Error in main function:", error);
+    return {error: "An error occurred in the main function"};
+  }
+});
